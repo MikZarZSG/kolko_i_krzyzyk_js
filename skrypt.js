27204ba11d513061a37190606c11c256eee6fb4b
MikZarZSG <mikzarzsg@gmail.com>
2020-10-12 08:58:26 +0200

    Akcja dla pol
    - Dodano akcje dla pol
    - Brak funkcji anonimowych
    - Dodano obrazki
    - Kolko i krzyzyk

--- skrypt.js
+++ skrypt.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+var pola = [];
+
+for(i = 0; i < 9; i++) {
+    var nazwa = "pole" + i;
+    pola.push(document.getElementById(nazwa));
+    pola[i].addEventListener("click", akcjaDlaPola.bind(null, i),  false);
+}
+
+function akcjaDlaPola(id) {
+    console.log(id);
+}
