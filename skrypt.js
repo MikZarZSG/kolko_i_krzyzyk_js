3b60256a12763184e43040fbe1f5db378563c37c
MikZarZSG <mikzarzsg@gmail.com>
2020-10-16 08:51:08 +0200

    Naprzemienie O i X

--- skrypt.js
+++ skrypt.js
@@ -1,19 +1,47 @@
+//Zmienne
 var pola = [];
+var odpowiedzi = new Array(9);
+var gracz = "O";
 
+var kolejka = document.querySelector(".kolejka");
+
+
+
+//Załadowanie strony
 for(i = 0; i < 9; i++) {
     var nazwa = "pole" + i;
     pola.push(document.getElementById(nazwa));
     pola[i].addEventListener("click", akcjaDlaPola.bind(null, i),  false);
 }
 
+kolejGracza(gracz);
+
+
+
+//Funkcje
 function akcjaDlaPola(id) {
-    console.log(id);
-    pola[id].classList.add("kolko");
-    //pola[id].classList.add("krzyzyk");
-    pola[id].classList.add("zablokowany");
+    oznaczPole(id);
+    kolejGracza(gracz);
     
     //Usunięcie listenera
     var nazwa = "pole" + id;
     var new_el = pola[id].cloneNode(false);
     pola[id].parentNode.replaceChild(new_el, pola[id]);
+}
+
+function oznaczPole(id) {
+    if(gracz == "O") {
+        pola[id].classList.add("kolko");
+        odpowiedzi[id] = "O";
+        gracz = "X";
+    } else if(gracz == "X") {
+        pola[id].classList.add("krzyzyk");
+        odpowiedzi[id] = "X";
+        gracz = "O";
+    }
+    pola[id].classList.add("zablokowany");
+}
+
+function kolejGracza(gracz) {
+    kolejka.innerHTML = "<h2>" + gracz + "</h2>";
 }
